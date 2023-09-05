0d7c253a793dd5a7769b02250efd1afa7cb4a2c7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-05 20:31:03 +0000

    ♻️  Refactor

--- calling_rate/number_of_burrows_from_call_rates.py
+++ calling_rate/number_of_burrows_from_call_rates.py
@@ -4,9 +4,8 @@ import numpy as np
 import pandas as pd
 
 
-def _xxget_burrow_coordinates(burrow_geci_data_path):
+def _xxget_burrow_coordinates(burrow_geci_data_path, burrow_jm_data_path):
     burrow_geci_data = pd.read_csv(burrow_geci_data_path)
-    burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
     burrow_jm_data = pd.read_csv(burrow_jm_data_path)
     merged_data = pd.concat([burrow_geci_data[["X", "Y"]], burrow_jm_data[["X", "Y"]]])
     return merged_data
@@ -14,17 +13,20 @@ def _xxget_burrow_coordinates(burrow_geci_data_path):
 
 def _get_burrow_coordinates():
     burrow_geci_data_path = "tests/data/coordenadas_madrigueras_geci.csv"
-    return _xxget_burrow_coordinates(burrow_geci_data_path)
+    burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
+    return _xxget_burrow_coordinates(burrow_geci_data_path, burrow_jm_data_path)
 
 
 def _get_number_of_burrows_in_burrow_area():
     burrow_geci_data_path = "tests/data/coordenadas_madrigueras_geci.csv"
-    return _xxget_burrow_coordinates(burrow_geci_data_path).shape[0]
+    burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
+    return _xxget_burrow_coordinates(burrow_geci_data_path, burrow_jm_data_path).shape[0]
 
 
 def _get_burrow_area():
     burrow_geci_data_path = "tests/data/coordenadas_madrigueras_geci.csv"
-    burrow_points = _xxget_burrow_coordinates(burrow_geci_data_path)
+    burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
+    burrow_points = _xxget_burrow_coordinates(burrow_geci_data_path, burrow_jm_data_path)
     return ConvexHull(burrow_points).volume
 
 
@@ -34,7 +36,8 @@ def get_density_in_burrow_area():
 
 def _get_burrow_polygon():
     burrow_geci_data_path = "tests/data/coordenadas_madrigueras_geci.csv"
-    burrow_points = _xxget_burrow_coordinates(burrow_geci_data_path)
+    burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
+    burrow_points = _xxget_burrow_coordinates(burrow_geci_data_path, burrow_jm_data_path)
     hull = ConvexHull(burrow_points)
     return burrow_points.iloc[hull.vertices, :]
 
