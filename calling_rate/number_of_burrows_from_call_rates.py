a597760e565f50da5ac3979d4d2971a28772a012
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-05 20:48:08 +0000

    ♻️  Refactor

--- calling_rate/number_of_burrows_from_call_rates.py
+++ calling_rate/number_of_burrows_from_call_rates.py
@@ -28,7 +28,7 @@ def get_density_in_burrow_area():
     ) / _get_burrow_area(burrow_geci_data_path, burrow_jm_data_path)
 
 
-def _xxget_burrow_polygon(burrow_geci_data_path, burrow_jm_data_path):
+def _get_burrow_polygon(burrow_geci_data_path, burrow_jm_data_path):
     burrow_points = _get_burrow_coordinates(burrow_geci_data_path, burrow_jm_data_path)
     hull = ConvexHull(burrow_points)
     return burrow_points.iloc[hull.vertices, :]
@@ -38,7 +38,7 @@ def is_inside_burrow_area(recorder_data_path):
     recorder_coordinates = get_recorder_coordinates(recorder_data_path)
     burrow_geci_data_path = "tests/data/coordenadas_madrigueras_geci.csv"
     burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
-    burrow_polygon = _xxget_burrow_polygon(burrow_geci_data_path, burrow_jm_data_path)
+    burrow_polygon = _get_burrow_polygon(burrow_geci_data_path, burrow_jm_data_path)
     return path.Path(burrow_polygon).contains_points(recorder_coordinates)
 
 
