2c733066e9acca3fa1457ea081b6ae0f60241e83
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-06 20:22:11 +0000

    ♻️  Refactor

--- calling_rate/raw_data_to_tdp.py
+++ calling_rate/raw_data_to_tdp.py
@@ -9,15 +9,10 @@ def get_recording_coordinates(shp_path):
 
 
 def get_recording_data(shp_path):
-    geojson_path = write_geojson_from_shp(shp_path)
-    recording_data_df = geojson_to_records_by_season_table(geojson_path)
-    return recording_data_df
-
-
-def write_geojson_from_shp(shp_path):
     geojson_path = "tmp.geojson"
     shp_files_to_geojson(shp_path, geojson_path)
-    return geojson_path
+    recording_data_df = geojson_to_records_by_season_table(geojson_path)
+    return recording_data_df
 
 
 def geojson_to_id_table(geojson_path: str):
@@ -32,6 +27,6 @@ def geojson_to_records_by_season_table(geojson_path):
     return geopandas_df.loc[:, ["Temporada", "id", "Estatus", "Pres-Ause", "Cant Voc", "Tasa Voc"]]
 
 
-def shp_files_to_geojson(raw_data: str, output_path: str):
+def shp_files_to_geojson(raw_data: str, output_path: str = "tmp.geojson"):
     geopandas_df = geopandas.read_file(raw_data)
     geopandas_df.to_file(output_path, driver="GeoJSON")
