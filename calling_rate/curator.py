723a235c711d027ec320ad0930815fc0923ff3ec
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-06 18:51:02 +0000

    ✅ Pass tests

--- calling_rate/curator.py
+++ calling_rate/curator.py
@@ -9,6 +9,8 @@ curator = typer.Typer(help="Tools to clean calling rates data")
 def write_recording_coordinates(shp_path: str, output_path: str):
     geojson_path = "tmp.geojson"
     calling_rate.shp_files_to_geojson(shp_path, geojson_path)
+    recording_coordinates_df = calling_rate.geojson_to_id_table(geojson_path)
+    recording_coordinates_df.to_csv(output_path, index=False)
 
 
 @curator.command()
