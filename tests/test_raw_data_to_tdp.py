9cd34b1b57bcbde7d0f0c443dd6c3c060b934c7c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-06 16:46:03 +0000

    🛑🧪 Fail tests

--- tests/test_raw_data_to_tdp.py
+++ tests/test_raw_data_to_tdp.py
@@ -3,4 +3,5 @@ from calling_rate import shp_files_to_geojson
 
 def test_shp_files_to_geojson():
     raw_path = "tests/data/nuevos_puntos_estimacion_poblacion2023.shp"
-    shp_files_to_geojson(raw_path)
+    geojson_path = "tests/data/geojson_from_shp.json"
+    shp_files_to_geojson(raw_path, geojson_path)
