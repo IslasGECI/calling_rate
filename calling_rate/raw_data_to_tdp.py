65af483ddbe562fb12ce15dbb2e077d8061828ed
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-06 18:01:20 +0000

    ✅ Pass tests

--- calling_rate/raw_data_to_tdp.py
+++ calling_rate/raw_data_to_tdp.py
@@ -9,5 +9,5 @@ def shp_files_to_geojson(raw_data: str, output_path: str):
 def geojson_to_id_table(geojson_path: str):
     geopandas_df = geopandas.read_file(geojson_path)
     geopandas_coordinates = geopandas_df.get_coordinates()
-    geopandas_df["X"] = geopandas_coordinates.x
+    geopandas_df[["X", "Y"]] = geopandas_coordinates
     return geopandas_df.loc[:, ["id", "X", "Y"]]
