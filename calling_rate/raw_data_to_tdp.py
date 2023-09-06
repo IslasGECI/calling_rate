e8b44eae7c19c6e7a97c00862c11d22a03bd1708
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-06 20:19:16 +0000

    ✅ Pass tests

--- calling_rate/raw_data_to_tdp.py
+++ calling_rate/raw_data_to_tdp.py
@@ -15,6 +15,13 @@ def geojson_to_id_table(geojson_path: str):
     return geopandas_df.loc[:, ["id", "X", "Y"]]
 
 
+def get_recording_data(shp_path):
+    geojson_path = "tmp.geojson"
+    shp_files_to_geojson(shp_path, geojson_path)
+    recording_data_df = geojson_to_records_by_season_table(geojson_path)
+    return recording_data_df
+
+
 def geojson_to_records_by_season_table(geojson_path):
     geopandas_df = geopandas.read_file(geojson_path)
     return geopandas_df.loc[:, ["Temporada", "id", "Estatus", "Pres-Ause", "Cant Voc", "Tasa Voc"]]
--- tests/test_raw_data_to_tdp.py
+++ tests/test_raw_data_to_tdp.py
@@ -2,7 +2,7 @@ from calling_rate import (
     geojson_to_id_table,
     geojson_to_records_by_season_table,
     get_recording_coordinates,
-    get_records_data,
+    get_recording_data,
     shp_files_to_geojson,
 )
 
@@ -52,6 +52,6 @@ def test_get_records_data():
     geojson_path = "tmp.geojson"
     if os.path.exists(geojson_path):
         os.remove(geojson_path)
-    get_records_data(raw_path)
+    get_recording_data(raw_path)
     assert os.path.exists(geojson_path)
     os.remove(geojson_path)
