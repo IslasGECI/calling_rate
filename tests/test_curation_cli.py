0b6f06f3e5a046810c884a8ea46e096c1fcba0e1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-06 18:48:40 +0000

    🛑🧪 Fail tests

--- tests/test_curation_cli.py
+++ tests/test_curation_cli.py
@@ -1,5 +1,7 @@
 from calling_rate import curator
+
 from typer.testing import CliRunner
+import os
 
 
 runner = CliRunner()
@@ -11,5 +13,10 @@ def tests_curator():
 
     shp_path = "tests/data/nuevos_puntos_estimacion_poblacion2023.shp"
     output_path = "tests/data/recording_coordinates.csv"
+
+    if os.path.exists(output_path):
+        os.remove(output_path)
     result = runner.invoke(curator, ["write-recording-coordinates", shp_path, output_path])
     assert result.exit_code == 0
+    assert os.path.exists(output_path)
+    # os.remove(output_path)
