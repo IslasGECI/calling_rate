5391b75f0e1e9d4a1d38e2ccee5a33f5031d1264
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-14 22:33:02 +0000

    ♻️  Refactor

--- calling_rate/number_of_burrows_from_call_rates.py
+++ calling_rate/number_of_burrows_from_call_rates.py
@@ -14,6 +14,10 @@ def _get_number_of_burrows_in_burrow_area(burrow_geci_data, burrow_jm_data):
 
 
 def _get_burrow_area(burrow_geci_data_path, burrow_jm_data_path):
+    return XX_get_burrow_area(burrow_geci_data_path, burrow_jm_data_path)
+
+
+def XX_get_burrow_area(burrow_geci_data_path, burrow_jm_data_path):
     burrow_geci_data = pd.read_csv(burrow_geci_data_path)
     burrow_jm_data = pd.read_csv(burrow_jm_data_path)
     burrow_points = _get_burrow_coordinates(burrow_geci_data, burrow_jm_data)
