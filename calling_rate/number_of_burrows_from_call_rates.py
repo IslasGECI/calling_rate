ef77452d86a392038353f346496077b019335520
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-14 21:41:59 +0000

    ♻️  Refactor

--- calling_rate/number_of_burrows_from_call_rates.py
+++ calling_rate/number_of_burrows_from_call_rates.py
@@ -46,6 +46,12 @@ def get_call_rate_in_burrow_area(recorder_data_path):
     return recorder_data.loc[is_recorder_inside, "Tasa_Voc"].mean()
 
 
+def XXget_call_rate_in_burrow_area(recorder_data_path):
+    is_recorder_inside = is_inside_burrow_area(recorder_data_path)
+    recorder_data = pd.read_csv(recorder_data_path)
+    return recorder_data.loc[is_recorder_inside, "Tasa_Voc"].mean()
+
+
 def get_call_rate_in_recorder_area(recorder_data):
     return recorder_data["Tasa_Voc"].mean()
 
