abc1a6f7b94b17ffad948c707ca2e56a000edf29
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-14 22:58:41 +0000

    ♻️  Refactor

--- calling_rate/number_of_burrows_from_call_rates.py
+++ calling_rate/number_of_burrows_from_call_rates.py
@@ -37,14 +37,14 @@ def is_inside_burrow_area(recorder_data, burrow_geci_data, burrow_jm_data):
 
 
 def get_call_rate_in_burrow_area(recorder_data):
-    return XXget_call_rate_in_burrow_area(recorder_data)
-
-
-def XXget_call_rate_in_burrow_area(recorder_data):
     burrow_geci_data_path = "tests/data/coordenadas_madrigueras_geci.csv"
     burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
     burrow_geci_data = pd.read_csv(burrow_geci_data_path)
     burrow_jm_data = pd.read_csv(burrow_jm_data_path)
+    return XXget_call_rate_in_burrow_area(recorder_data, burrow_geci_data, burrow_jm_data)
+
+
+def XXget_call_rate_in_burrow_area(recorder_data, burrow_geci_data, burrow_jm_data):
     is_recorder_inside = is_inside_burrow_area(recorder_data, burrow_geci_data, burrow_jm_data)
     return recorder_data.loc[is_recorder_inside, "Tasa_Voc"].mean()
 
