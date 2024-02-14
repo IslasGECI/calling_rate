434088dfba5dbe3952bd828d4d21ba11bfb04176
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-14 22:46:37 +0000

    ♻️  Refactor

--- calling_rate/number_of_burrows_from_call_rates.py
+++ calling_rate/number_of_burrows_from_call_rates.py
@@ -31,6 +31,10 @@ def _get_burrow_polygon(burrow_geci_data, burrow_jm_data):
 
 
 def is_inside_burrow_area(recorder_data):
+    return XXis_inside_burrow_area(recorder_data)
+
+
+def XXis_inside_burrow_area(recorder_data):
     recorder_coordinates = get_recorder_coordinates(recorder_data)
     burrow_geci_data_path = "tests/data/coordenadas_madrigueras_geci.csv"
     burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
