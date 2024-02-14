705f9e32be2b949b322be86961027802b5339ea6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-14 22:38:27 +0000

    ♻️  Refactor

--- tests/test_number_of_burrows_from_call_rates.py
+++ tests/test_number_of_burrows_from_call_rates.py
@@ -2,7 +2,7 @@ from calling_rate import (
     get_area_for_each_recorder,
     get_call_rate_in_burrow_area,
     get_call_rate_in_recorder_area,
-    get_density_in_burrow_area,
+    XXget_density_in_burrow_area,
     get_density_in_recorder_area,
     get_number_of_burrows_in_recorder_area,
     get_number_of_recorders,
@@ -20,7 +20,9 @@ def test_get_density_in_burrow_area():
     expected_density = 3.559479030607237e-5
     burrow_geci_data_path = "tests/data/coordenadas_madrigueras_geci.csv"
     burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
-    obtained_density = get_density_in_burrow_area(burrow_geci_data_path, burrow_jm_data_path)
+    burrow_geci_data = pd.read_csv(burrow_geci_data_path)
+    burrow_jm_data = pd.read_csv(burrow_jm_data_path)
+    obtained_density = XXget_density_in_burrow_area(burrow_geci_data, burrow_jm_data)
     assert obtained_density == expected_density
 
 
