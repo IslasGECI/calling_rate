80daa52ca18f345612c16e651d6cd6cd3ce3adcd
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-16 19:49:13 +0000

    🛑🧪 Fail tests

---
+++ tests/test_RateCalling_Burrow_Data.py
@@ -0,0 +1,17 @@
+from calling_rate import RateCalling_Burrow_Data
+
+
+def test_ratecalling_bootstrapping():
+
+    recorder_data_path = "tests/data/puntos_grabaciones_estimacion_poblacion_nuevos.csv"
+    burrow_geci_data_path = "tests/data/coordenadas_madrigueras_geci.csv"
+    burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
+    paths = {
+        "recorders_data": recorder_data_path,
+        "geci_data": burrow_geci_data_path,
+        "jm_data": burrow_jm_data_path,
+    }
+    ratecalling_burrow_data = RateCalling_Burrow_Data(paths)
+    obtained = ratecalling_burrow_data.bootstrapping()
+    expected_number_samples = 85
+    assert_the_nrow_is_the_same_to_the_original(obtained)
