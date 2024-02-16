566eab89e308b411be302ba31cece613a5357a55
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-02-16 21:36:57 +0000

    ♻️  Refactor

--- tests/test_RateCalling_Burrow_Data.py
+++ tests/test_RateCalling_Burrow_Data.py
@@ -28,7 +28,7 @@ def test_ratecalling_bootstrapping():
     ratecalling_burrow_data = RateCalling_Burrow_Data(paths)
     obtained = ratecalling_burrow_data.bootstrapping()
     assert_the_nrow_is_the_same_to_the_original(obtained)
-    assert_the_first_id_is_different_to_64(obtained)
+    assert_the_first_id_is_different_to_original_first_id(obtained)
     assert_the_first_id_is_stable(obtained)
 
 
@@ -37,7 +37,7 @@ def assert_the_nrow_is_the_same_to_the_original(obtained):
     assert len(obtained) == expected_number_samples
 
 
-def assert_the_first_id_is_different_to_64(obtained):
+def assert_the_first_id_is_different_to_original_first_id(obtained):
     original_first_id = 64
     obtained_first_id = obtained.ID_punto.values[0]
     assert obtained_first_id != original_first_id
