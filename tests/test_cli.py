680270e626ddd82ebee6f3effeaf58bfc5db5aa1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-14 21:17:55 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 from calling_rate import cli
+import test_tools as tt
 
 from typer.testing import CliRunner
 
@@ -10,5 +11,11 @@ def tests_cli():
     result = runner.invoke(cli, "--help")
     assert result.exit_code == 0
 
-    result = runner.invoke(cli, ["write-initial-population", "--bootstrapping-number", 10])
+    output_path = "tests/data/initial_population.json"
+    tt.if_exist_remove(output_path)
+    result = runner.invoke(
+        cli,
+        ["write-initial-population", "--output-path", output_path, "--bootstrapping-number", 10],
+    )
     assert result.exit_code == 0
+    tt.assert_exist(output_path)
