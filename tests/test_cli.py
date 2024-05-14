3c29ad69c96e8b3d54e5e808594565f71fd6afab
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-14 21:11:16 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,3 +9,6 @@ runner = CliRunner()
 def tests_cli():
     result = runner.invoke(cli, "--help")
     assert result.exit_code == 0
+
+    result = runner.invoke(cli, ["write-initial-population", "--bootstrapping_number", 10])
+    assert result.exit_code == 0
