a3b8d8760a6231807e181954c3470be00cfc19b6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-14 21:30:33 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,9 +13,18 @@ def tests_cli():
 
     output_path = "tests/data/initial_population.json"
     tt.if_exist_remove(output_path)
+    burrow_jm_data_path = "tests/data/coordenadas_madrigueras_jm.csv"
     result = runner.invoke(
         cli,
-        ["write-initial-population", "--output-path", output_path, "--bootstrapping-number", 10],
+        [
+            "write-initial-population",
+            "--output-path",
+            output_path,
+            "--bootstrapping-number",
+            10,
+            "--burrow-jm-data-path",
+            burrow_jm_data_path,
+        ],
     )
     assert result.exit_code == 0
     tt.assert_exist(output_path)
