2a0aa6a5ab9aeb0f27bf2a2840fd778bc442ba99
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-14 21:14:26 +0000

    ✅ Pass tests

--- calling_rate/cli.py
+++ calling_rate/cli.py
@@ -6,6 +6,11 @@ cli = typer.Typer(help="Tools to calculate initial population")
 
 
 @cli.command()
+def write_initial_population(bootstrapping_number: int = typer.Option()):
+    pass
+
+
+@cli.command()
 def version():
     version = calling_rate.__version__
     print(version)
