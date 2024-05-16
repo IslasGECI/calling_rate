7b30d61d7c13264b1ab42db7c5ef77b5d2c5c370
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2024-05-16 11:26:28 -0700

    🎨 format with black

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,6 +12,7 @@ def tests_cli_version():
     result = runner.invoke(cli, "version")
     assert result.stdout == "0.4.0\n"
 
+
 def tests_cli():
     result = runner.invoke(cli, "--help")
     assert result.exit_code == 0
