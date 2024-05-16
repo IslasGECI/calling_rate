f1cac6204bf296db40c0efa756ad0a7c36672973
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-05-16 19:01:07 +0000

    ♻️  Refactor

--- calling_rate/cli.py
+++ calling_rate/cli.py
@@ -24,18 +24,18 @@ def write_initial_population(
     }
     ratecalling_burrow_data = calling_rate.RateCalling_Burrow_Data(paths, B=bootstrapping_number)
     interval = ratecalling_burrow_data.get_bootstrapped_number_of_burrows_in_recorder_area()
-    command_name = inspect.stack()[0][3]
     dict_to_write = {
         "b_number": bootstrapping_number,
         "intervals": list(interval),
         "data_sources": paths,
-        "get": get_command_reference(command_name),
+        "get": get_command_reference(),
     }
     with open(output_path, "w") as jsonfile:
         json.dump(dict_to_write, jsonfile)
 
 
-def get_command_reference(command_name):
+def get_command_reference():
+    command_name = inspect.stack()[1][3]
     return (
         calling_rate.__name__ + "." + os.path.basename(__file__).split(".")[0] + ":" + command_name
     )
